b04d6803818aae69087cd8a77320b4f305f84a16
Beatriz <90735264+trizpasser@users.noreply.github.com>
2023-11-14 01:56:16 -0300

    bug fix

--- src/service/EmailService.py
+++ src/service/EmailService.py
@@ -9,8 +9,7 @@ def envia_email(email, assunto, mensagem):
     response_mock = Mock()
     response_mock.status_code = "Email enviado", 200
     response_mock.json.return_value = "Email enviado com sucesso!"
-    
-    email_existe = True
+
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b' # expressão para validar o formato do email
 
     if not (re.fullmatch(regex, email)): # confere se está dentro do formato
@@ -24,23 +23,6 @@ def envia_email(email, assunto, mensagem):
         ]
 
         return response_mock.json()
-
-    if email_existe:
-       
-       return response_mock.json()
-
-    else: 
-        response_mock = Mock()
-        response_mock.status_code = 422
-        response_mock.json.return_value = [
-            {
-                "codigo": 404,
-                "mensagem": "Email não existe."
-            }
-        ]
-
-        return response_mock.json()
     
-    
-
     return response_mock.json()
+    
