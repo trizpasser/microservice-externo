dc183f340cb3f1117fa8a9303c2a1d4036774f49
Beatriz <90735264+trizpasser@users.noreply.github.com>
2023-11-14 07:38:01 -0300

    fix email_test

--- src/service/EmailService.py
+++ src/service/EmailService.py
@@ -7,26 +7,22 @@ def envia_email(email, assunto, mensagem):
     response_mock = Mock()
     response_mock.status_code = "Email enviado", 200
    
-    response_mock.json.return_value = [
-        {
-            "msg": "Email enviado com sucesso!",
-            "email": email,
-            "assunto": assunto,
-            "mensagem": mensagem,
-        }
-    ]
+    response_mock.json.return_value = {
+        "msg": "Email enviado com sucesso!",
+        "email": email,
+        "assunto": assunto,
+        "mensagem": mensagem,
+    }
 
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b' # expressão para validar o formato do email
 
     if not (re.fullmatch(regex, email)): # confere se está dentro do formato
         response_mock = Mock()
         response_mock.status_code = 422
-        response_mock.json.return_value = [
-            {
-                "codigo": 422,
-                "mensagem": "Email com formato inválido."
-            }
-        ]
+        response_mock.json.return_value = {
+            "codigo": 422,
+            "mensagem": "Email com formato inválido."
+        }
 
         return response_mock.json()
     
