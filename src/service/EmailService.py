0f9a0130f73b1ad2547968a197e4ace552f9cc61
Beatriz <90735264+trizpasser@users.noreply.github.com>
2023-11-14 01:52:59 -0300

    bug fix

--- src/service/EmailService.py
+++ src/service/EmailService.py
@@ -25,7 +25,11 @@ def envia_email(email, assunto, mensagem):
 
         return response_mock.json()
 
-    if not email_existe:
+    if email_existe:
+       
+       return response_mock.json()
+
+    else: 
         response_mock = Mock()
         response_mock.status_code = 422
         response_mock.json.return_value = [
@@ -40,45 +44,3 @@ def envia_email(email, assunto, mensagem):
     
 
     return response_mock.json()
-
-
-
-
-
-    '''
-    servidor_smtp = "smtp.mail.yahoo.com"
-    porta_smtp = 465
-    usuario_smtp = "vaidebike3@yahoo.com.br"
-    senha_smtp = os.environ.get('SENHA_EMAIL')  # Usar uma variável de ambiente para a senha
-
-    remetente = usuario_smtp
-    assunto = "Notificação"
-    mensagem = "Você está sendo notificado pelo Vai de Bike!"
-
-    corpo_email = f"Assunto: {assunto}\n\n{mensagem}"
-
-    msg = MIMEText(corpo_email)
-    msg['Subject'] = assunto
-    msg['From'] = remetente
-    msg['To'] = destinatario
-
-    try:
-        
-        with smtplib.SMTP_SSL(servidor_smtp, porta_smtp) as server:
-            server.login(usuario_smtp, senha_smtp)
-            server.sendmail(remetente, destinatario, msg.as_string())
-
-        # supostamente envia email
-
-        logging.info(f'E-mail enviado para {destinatario} com sucesso!')
-        return 'E-mail enviado com sucesso!'
-    except smtplib.SMTPAuthenticationError:
-        logging.error("Erro de autenticação SMTP. Verifique as credenciais.")
-        return "Erro de autenticação SMTP. Verifique as credenciais."
-    except smtplib.SMTPException as e:
-        logging.error(f"Erro ao enviar e-mail para {destinatario}: {str(e)}")
-        return f"Erro ao enviar e-mail: {str(e)}"
-        '''
-    
-    
-
