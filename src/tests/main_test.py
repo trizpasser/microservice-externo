ba26c3c2e9dcdde8dea5ae345bd6e33454571c6d
Juan Gomes <juangsantanna@gmail.com>
2023-11-21 14:04:37 -0300

    hot fix

--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
 from flask import Flask
+import os, sys
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 from controller.main import app, email, cobranca  # Substitua "seu_modulo" pelo nome real do seu módulo
